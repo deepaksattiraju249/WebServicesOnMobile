1fddc2ebe4a3f82a2f6aa42d891ad509b90dea09
deepaksattiraju249 <deepaks.iiti@gmail.com>
2014-09-25 14:35:17 +0530

    Added functions

--- XMPP_Client/src/Client/XMPPClient.java
+++ XMPP_Client/src/Client/XMPPClient.java
@@ -1,5 +1,16 @@
 package Client;
 
+
+import java.util.Collection;
+import java.util.Iterator;
+
+import org.jivesoftware.smack.ChatManager;
+import org.jivesoftware.smack.RosterGroup;
+import org.jivesoftware.smack.SmackConfiguration;
+import org.jivesoftware.smack.XMPPConnection;
+import org.jivesoftware.smack.XMPPException;
+import org.jivesoftware.smack.ConnectionConfiguration.SecurityMode;
+import org.jivesoftware.smack.packet.Presence.Type;
 import org.jivesoftware.smack.Chat;
 import org.jivesoftware.smack.ConnectionConfiguration;
 import org.jivesoftware.smack.MessageListener;
@@ -14,45 +25,187 @@ import org.jivesoftware.smack.packet.Presence;
 
 class XMPPClient {
 	
-	ConnectionConfiguration config=null;
-	XMPPConnection xmppCon=null;
+	
+	  private static final int packetReplyTimeout = 500; // millis
+	    
+	    public String server;
+	    public int port;
+	    public String servername;
+	    
+	    int buddySize = 0;
+	    String[] buddies = null;
+	    
+	    private ConnectionConfiguration config;
+	    private XMPPConnection connection;
+
+	    private ChatManager chatManager;
+	    private MessageListener messageListener;
+	
+	
 	
 	XMPPClient()
 	{			
-			config = new ConnectionConfiguration("10.200.40.153",5222,"vysper.org");
-			
-	
-		//config = new ConnectionConfiguration("vysper.org");
+		SmackConfiguration.setPacketReplyTimeout(packetReplyTimeout);
+		//Setup Initialization
+		config = new ConnectionConfiguration("10.200.40.153",5222,"vysper.org");
 		config.setSecurityMode(ConnectionConfiguration.SecurityMode.enabled);
 		config.setSASLAuthenticationEnabled(true);
 		config.setSelfSignedCertificateEnabled(true);
-        config.setKeystorePath("/XMPP_Client/src/Client/bogus_mina_tls.cert");
-        config.setTruststorePath("/XMPP_Client/src/Client/bogus_mina_tls.cert");
+        config.setKeystorePath("src/Client/bogus_mina_tls.cert");
+        config.setTruststorePath("src/Client/bogus_mina_tls.cert");
 		config.setTruststorePassword("boguspw");
-		xmppCon = new XMPPConnection(config);
+		connection = new XMPPConnection(config);
 		
 		try
 		{
-			xmppCon.connect();
-			xmppCon.login("user1@vysper.org","pass1");
-			
-			
+			connection.connect();
+			connection.login("user1@vysper.org","user1");
 		}
 		catch(XMPPException ex)
 		{
 			System.out.println("=============Error=============");
 			System.out.println(ex);
-			System.out.println("Check the connection or Credentials");
+			System.out.println("Check the connection");
 			System.out.println("===============================");			
 		}
 		
-		
-		
-		
-		
-		
+		System.out.println("Connection Successful");		
 		
 		
 	}
+	
+	public void setStatus(boolean available, String status) 
+	{
+        
+        Presence.Type type = available? Type.available: Type.unavailable;
+        Presence presence = new Presence(type);
+        
+        presence.setStatus(status);
+        connection.sendPacket(presence);
+     
+    }
+	
+	public void closeConnection()
+	{
+	    	connection.disconnect();
+	}
 
+	
+	public void destroy()
+	{
+        if (connection!=null && connection.isConnected()) 
+        {
+            connection.disconnect();
+        }
+    }
+	
+	
+	public void sendMessage(String message, String buddyJID) throws XMPPException
+	{
+        System.out.println(String.format("Sending mesage '%1$s' to user %2$s", message, buddyJID));
+        Chat chat = chatManager.createChat(buddyJID, messageListener);
+        chat.sendMessage(message);
+    }
+	
+	
+	public void createEntry(String user, String name, String[] groupn) throws Exception
+	{
+        System.out.println(String.format("Creating entry for buddy '%1$s' with name %2$s", user, name));
+        Roster roster = connection.getRoster();
+        roster.createEntry(user, name, groupn);
+    }
+	
+	 public void createGroup(String groupName, String userName, String nickName) throws XMPPException
+	 {
+	    	Roster roster = connection.getRoster();
+	    	RosterGroup group = roster.createGroup(groupName);
+			roster.createEntry(userName, nickName, new String[] { group.getName() });
+	 }
+	    
+	 public void addUserToGroup(String[] groupName, String userName, String nickName) throws XMPPException
+	 {
+	    	Roster roster = connection.getRoster();
+	    	roster.createEntry(userName, nickName, groupName);
+	 }
+	 public void getRosterDetail(String groupName)
+	 {
+	    	Roster roster = connection.getRoster();
+	    	RosterGroup rostergroup = roster.getGroup(groupName);
+	    	Collection<RosterEntry> entries = rostergroup.getEntries();
+	    	Iterator<RosterEntry> iter = entries.iterator();
+	    	buddySize = entries.size();  
+	        buddies = new String[buddySize];
+	        int i = 0;
+	        System.out.println(rostergroup.getName() + " have subscribed users: " + rostergroup.getEntryCount());
+	        while (iter.hasNext()) 
+	        {
+	            RosterEntry rosterEntry = (RosterEntry) iter.next();
+	            buddies[i] = rosterEntry.getUser();
+	            i++;
+	            System.out.println(rosterEntry.getUser());          
+	        }
+	    	
+	  }
+	 
+	 public void getAllRosterGroup()
+	 {
+	    	Roster roster = connection.getRoster();
+	    	Collection<RosterGroup> entries = roster.getGroups();
+	    	Iterator<RosterGroup> iter = entries.iterator();
+	    	buddySize = entries.size();  
+	        buddies = new String[buddySize];
+	        int i = 0;
+	        System.out.println("Groups formed: ");
+	        while (iter.hasNext()) 
+	        {
+	            RosterGroup rostergroup = (RosterGroup) iter.next();
+	            buddies[i] = rostergroup.getName();
+	            i++;
+	            System.out.println(rostergroup.getName());          
+	        }
+	   }
+	 public void getUserRoster(String userName)
+	 {
+	    	Roster roster = connection.getRoster();
+	    	RosterEntry rosterentry = roster.getEntry(userName);
+	    	Collection<RosterGroup> entries = rosterentry.getGroups();
+	    	Iterator<RosterGroup> iter = entries.iterator();
+	    	
+	    	buddySize = entries.size();  
+	        buddies = new String[buddySize];
+	        int i = 0;
+	        System.out.println("Groups Belong to: " + userName);
+	        while (iter.hasNext()) {
+	            RosterGroup rostergroup = (RosterGroup) iter.next();
+	            buddies[i] = rostergroup.getName();
+	            i++;
+	            System.out.println(rostergroup.getName());
+	        }
+	    	
+	    	
+	 }
+	 
+	 
+	 public void displayBuddy()
+	 {
+	        Roster roster = connection.getRoster();
+	        Collection<RosterEntry> entries = roster.getEntries();
+	        System.out.println("Total Providers!!");
+	        System.out.println(entries.size() + " Providers \n");
+	        Iterator<RosterEntry> iter = entries.iterator();
+	        buddySize = entries.size();  
+	        buddies = new String[buddySize];
+	        int i = 0;
+	        while (iter.hasNext()) 
+	        {
+	            RosterEntry rosterEntry = (RosterEntry) iter.next();
+	            buddies[i] = rosterEntry.getUser();
+	            i++;
+	            System.out.println(i + ". " + rosterEntry.getUser());          
+	        }
+	        System.out.println("----------");
+	    }
+	 
+	 
+	
 }
