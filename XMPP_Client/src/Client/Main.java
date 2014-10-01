6247bf3e8229bd71dbe90cf434cc9665284d3a08
deepaksattiraju249 <deepaks.iiti@gmail.com>
2014-10-01 16:21:18 +0530

    Main

--- XMPP_Client/src/Client/Main.java
+++ XMPP_Client/src/Client/Main.java
@@ -1,11 +1,11 @@
 package Client;
-
+import Client.XMPP_Client;
 
 public class Main {
-	public static void main(String args[])
+	public static void main(String args[]) throws Exception
 	{
 		
-		
+		XMPP_Client client = new XMPP_Client("user1","pass1");
 		
 	}
 
